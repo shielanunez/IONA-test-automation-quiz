3be671e959d4819c648c6904c8f3cd898c52064a
shielamarienunez <shiela.nunez@jfcvendor.com>
2026-09-11 20:33:13 +0800

    Update Navbar.ts

--- playwright/shielaNunez/pages/component/Navbar.ts
+++ playwright/shielaNunez/pages/component/Navbar.ts
@@ -1,15 +1,27 @@
-import { Page, Locator } from '@playwright/test'
+import { Page, Locator, expect } from '@playwright/test'
 
 export class NavBar {
     private readonly page: Page;
-    private readonly cartLink: Locator
+    private readonly cartLink: Locator;
+    private readonly loginLink: Locator;
+    private readonly nameOfUser: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.cartLink = this.page.getByRole('link', { name: 'Cart', exact: true });
+        this.loginLink = this.page.getByRole('link', { name: 'Log in', exact: true });
+        this.nameOfUser = this.page.locator('#nameofuser');
     }
 
     async navigateToCart() {
         await this.cartLink.click();
+    }
+
+    async openLoginModal(){
+        await this.loginLink.click();
+    }
+
+    async verifyNameOfUser(username: string){
+        await expect(this.nameOfUser).toContainText(`Welcome ${username}`);
     }
 }
